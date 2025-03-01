33f87a629e636f74c630ca21796be05845714b6d
Bruna <brn.flor@gmail.com>
2025-03-01 15:46:40 +0000

    Adding player movement

--- code/Const.py
+++ code/Const.py
@@ -12,6 +12,7 @@ ENTITY_SPEED = {
     'Level1Bg4': 4,
     'Level1Bg5': 5,
     'Level1Bg6': 6,
+    'Player1':3
 }
 # Texts
 MENU_OPTION = ('NEW GAME 1P', 'NEW GAME 2P - COOPERATIVE', 'NEW GAME 2P - COMPETITIVE', 'SCORE', 'EXIT')
--- code/Player.py
+++ code/Player.py
@@ -1,3 +1,6 @@
+import pygame
+
+from code.Const import ENTITY_SPEED, WIN_HEIGHT, WIN_WIDTH
 from code.Entity import Entity
 
 
@@ -5,8 +8,18 @@ class Player(Entity):
     def __init__(self, name:str, position:tuple):
         super().__init__(name, position)
 
-    def update(self):
-        pass
-
     def move(self):
-        pass
+        # On press key
+        pressed_key = pygame.key.get_pressed()
+        # Top
+        if pressed_key[pygame.K_UP] and self.rect.top > 0:
+            self.rect.centery -= ENTITY_SPEED[self.name]
+        # Bottom
+        if pressed_key[pygame.K_DOWN] and self.rect.bottom < WIN_HEIGHT:
+            self.rect.centery += ENTITY_SPEED[self.name]
+        # Left
+        if pressed_key[pygame.K_LEFT] and self.rect.left > 0:
+            self.rect.centerx -= ENTITY_SPEED[self.name]
+        # Right
+        if pressed_key[pygame.K_RIGHT] and self.rect.bottom < WIN_WIDTH:
+            self.rect.centerx += ENTITY_SPEED[self.name]
