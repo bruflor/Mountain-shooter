2dc87e04b191533ff00ef8f22842b71ceaeb1ecb
Bruna <brn.flor@gmail.com>
2025-03-08 13:06:55 +0000

    Passing score through levels

--- code/Level.py
+++ code/Level.py
@@ -16,7 +16,7 @@ from code.Player import Player
 
 
 class Level:
-    def __init__(self, window, name, game_mode):
+    def __init__(self, window:Surface, name:str, game_mode:str, player_score:list[int]):
         self.timeout = TIMEOUT_LEVEL
         self.window = window
         self.name = name
@@ -26,16 +26,20 @@ class Level:
         # Getting all bg
         self.entity_list.extend(EntityFactory.get_entity(self.name+'Bg'))
         # Adding the player
-        self.entity_list.append(EntityFactory.get_entity('Player1'))
+        player = EntityFactory.get_entity('Player1')
+        player.score = player_score[0]
+        self.entity_list.append(player)
         # game mode is 2 players
         if game_mode in [MENU_OPTION[1], MENU_OPTION[2]]:
-            self.entity_list.append(EntityFactory.get_entity('Player2'))
+            player = EntityFactory.get_entity('Player2')
+            player.score = player_score[1]
+            self.entity_list.append(player)
 
         # Defining enemies by time
         pygame.time.set_timer(EVENT_ENEMY, SPAWN_TIME)
         pygame.time.set_timer(EVENT_TIMEOUT, TIMEOUT_STEP) # check victory condition
 
-    def run(self, ):
+    def run(self, player_score):
         pygame.mixer_music.load(f'./asset/{self.name}.mp3')
         pygame.mixer_music.play(-1)
         clock = pygame.time.Clock()  # FPS
@@ -63,8 +67,21 @@ class Level:
                 if event.type == EVENT_TIMEOUT:
                     self.timeout -= TIMEOUT_STEP
                     if self.timeout == 0:
+                        # updating score before timeout
+                        for ent in self.entity_list:
+                            if isinstance(ent, Player) and ent.name == 'Player1':
+                                player_score[0] = ent.score
+                            if isinstance(ent, Player) and ent.name == 'Player2':
+                                player_score[1] = ent.score
                         return True
 
+                # If we do not found any player because it was killed, return false to end game
+                found_player = False
+                for ent in self.entity_list:
+                    if isinstance(ent, Player):
+                        found_player = True
+                if not found_player:
+                    return False
             # Texts
             self.level_text(14, f'{self.name} - Timeout {self.timeout / 1000 : .1f}s', COLOR_WHITE, (10, 5))
             self.level_text(14, f'fps: {clock.get_fps() : .0f}', COLOR_WHITE, (10, WIN_HEIGHT - 35))
