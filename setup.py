90376b280aa36ef5144592a51670a7c10d1690fa
Bruna <brn.flor@gmail.com>
2025-03-23 15:20:30 +0000

    setup to cxfreeze

--- setup.py
+++ setup.py
@@ -2,6 +2,10 @@ import os
 
 from cx_Freeze import Executable, setup
 
+build_exe_options = {
+    "packages": ["pygame"],  
+    "include_files": ["asset/"],
+}
 
 executables = [Executable("main.py")]
 
@@ -9,6 +13,6 @@ setup(
     name="MountainShooter",
     version="1.0.0",
     description="Mountain Shooter arcade game",
-    options={"build_exe": {"packages": ["pygame"]}},
+    options={"build_exe": build_exe_options},
     executables=executables,
 )
